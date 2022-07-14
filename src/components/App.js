25aa15115a65eeff1ef1376b222532ee4dcf9e3c
ghofran <gho0ofran.adel14@gmail.com>
2022-07-14 21:20:29 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+
+import { useState, useEffect } from 'react'
+
+export default function App() {
+  const [image, setImage] = useState()
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => setImage(data.message))
+  }, [])
+
+  if (!image) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <>
+      <img src={image} alt="A Random Dog" />
+    </>
+  )
+}
+
